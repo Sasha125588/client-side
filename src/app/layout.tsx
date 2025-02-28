c39258a1ae1fee2af223cc8db8f12d2b7305467a
sasha125588 <sashavolskiy11@gmail.com>
2025-02-28 21:42:45 +0200

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
 import { NextIntlClientProvider } from 'next-intl'
 import { getLocale, getMessages } from 'next-intl/server'
@@ -37,6 +38,7 @@ export default async function RootLayout({
 				<NextIntlClientProvider messages={messages}>
 					<Header />
 					{children}
+					<Analytics />
 				</NextIntlClientProvider>
 			</body>
 		</html>
